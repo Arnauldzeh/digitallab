463020af76765386cbee6b011c5a31b37d2b522c
Arnauldzeh <arnauldzeh740@gmail.com>
2025-08-29 19:11:06 +0100

    refactor vercel

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
-const mongoose = require("mongoose");
 
-const startServer = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
-
-    if (process.env.IS_LOCAL === "true") {
-      const port = process.env.PORT || 5000;
-      app.listen(port, () => {
-        console.log(`🚀 Server running locally on port ${port}`);
-      });
-    }
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
-    process.exit(1); // Arrête si Mongo ne marche pas
-  }
-};
-
-startServer();
-
+if (process.env.IS_LOCAL === "true") {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running locally on port ${port}`);
+  });
+}
+//n llllrdrrxtxt
 module.exports = app;
